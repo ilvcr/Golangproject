89b819bb395d48b2d7f33ccc21495bd51d629d6f
Yoghourt <liyaoliu@foxmail.com>
2019-03-18 14:12:46 +0800

    add py script

---
+++ addoneonacolinpyoli.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 18 09:54:06 2019
+
+@author: gao->ilvcr
+"""
+
+__author__ = 'gao->ilvcr'
+
+
+
+#inpathsource = 'C:\\Users\\Administrator\\Desktop\\indata.txt'
+#outpathsource = 'C:\\Users\\Administrator\\Desktop\\outdata.txt'
+
+def addoneonacol():
+    #reqlist = []
+    for i in range(208):
+        print(i * 2.28)
+                
+                       
+if __name__ == '__main__':
+    addoneonacol()
+    
+    
