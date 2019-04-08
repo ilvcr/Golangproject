e68f416430e3a922a666c4a13beed13b76167516
Yoghourt <liyaoliu@foxmail.com>
2019-04-08 22:53:08 +0800

    g

---
+++ imagescript/tensorflow/generateTFRecord.py
@@ -0,0 +1,55 @@
+﻿# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 08 21:12:15 2019
+
+@author: gao->ilvcr
+
+Description： 使用tf.train.Example来定义我们要填入的数据格式，
+              然后使用tf.python_io.TFRecordWriter来写入TFRecords文件
+"""
+
+__author__ = "yoghourt->gao"
+
+
+import os
+import tensorflow as tf 
+from PIL import Image
+
+cwd = os.getcwd()
+
+'''
+此处我加载的数据目录如下：
+-- img1.jpg
+     img2.jpg
+     img3.jpg
+     ...
+-- img1.jpg
+     img2.jpg
+     ...
+-- ...
+...
+'''
+writer = tf.python_io.TFRecordWriter("train.tfrecords")
+for index, name in enumerate(classes):
+    class_path = cwd + name + "/"
+    for img_name in os.listdir(class_path):
+        img_path = class_path + img_name
+            img = Image.open(img_path)
+            img = img.resize((224, 224))
+        img_raw = img.tobytes()              #将图片转化为原生bytes
+        example = tf.train.Example(features=tf.train.Features(feature={
+            "label": tf.train.Feature(int64_list=tf.train.Int64List(value=[index])),
+            'img_raw': tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_raw]))
+        }))
+        writer.write(example.SerializeToString())  #序列化为字符串
+writer.close()
+
+
+for serialized_example in tf.python_io.tf_record_iterator("train.tfrecords"):
+    example = tf.train.Example()
+    example.ParseFromString(serialized_example)
+
+    image = example.features.feature['image'].bytes_list.value
+    label = example.features.feature['label'].int64_list.value
+    # 可以做一些预处理之类的
+    print image, label
---
+++ imagescript/tensorflow/readTFRecordsusingQueue.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 08 22:18:15 2019
+
+@author: gao->ilvcr
+
+Description： 生成了TFRecords文件后，接下来就可以使用队列（queue）读取数据了
+"""
+
+__author__ = "yoghourt->gao"
+
+
+def read_and_decode(filename):
+    #根据文件名生成一个队列
+    filename_queue = tf.train.string_input_producer([filename])
+
+    reader = tf.TFRecordReader()
+    _, serialized_example = reader.read(filename_queue)   #返回文件名和文件
+    features = tf.parse_single_example(serialized_example,
+                                       features={
+                                           'label': tf.FixedLenFeature([], tf.int64),
+                                           'img_raw' : tf.FixedLenFeature([], tf.string),
+                                       })
+
+    img = tf.decode_raw(features['img_raw'], tf.uint8)
+    img = tf.reshape(img, [224, 224, 3])
+    img = tf.cast(img, tf.float32) * (1. / 255) - 0.5
+    label = tf.cast(features['label'], tf.int32)
+
+    return img, label
+
+
+#使用方式
+img, label = read_and_decode("train.tfrecords")
+
+#使用shuffle_batch可以随机打乱输入
+img_batch, label_batch = tf.train.shuffle_batch([img, label],
+                                                batch_size=30, capacity=2000,
+                                                min_after_dequeue=1000)
+init = tf.initialize_all_variables()
+
+with tf.Session() as sess:
+    sess.run(init)
+    threads = tf.train.start_queue_runners(sess=sess)
+    for i in range(3):
+        val, l= sess.run([img_batch, label_batch])
+        #我们也可以根据需要对val， l进行处理
+        #l = to_categorical(l, 12) 
+        print(val.shape, l)
