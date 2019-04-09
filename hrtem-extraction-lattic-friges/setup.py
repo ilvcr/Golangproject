b94ca5d72c03a8256a4f5a908f94808388c45a52
gao <leeyog@163.com>
2019-04-09 16:24:53 +0800

    g

--- hrtem-extraction-lattic-friges/setup.py
+++ hrtem-extraction-lattic-friges/setup.py
@@ -6,7 +6,21 @@
 # Author: yoghourt->ilvcr 
 # Mail: liyaoliu@foxmail.com  @@  ilvcr@outlook.com 
 # Created Time: Tue Apr  9 16:29:57 2018
-# Description: 
+# Description: 创建源代码压缩包或安装软件
 #************************************************************************#
 
 
+"""hrtem-extraction-lattic-friges project"""
+from setuptools import find_packages, setup
+
+setup(name = 'hrtem-extraction-lattic-friges',
+    version = '0.1.1',
+    description = "hrtem-extraction-lattic-friges.",
+    long_description = "hrtem-extraction-lattic-friges.",
+    platforms = ["Linux"],
+    author="yoghourt->ilvcr",
+    author_email="liyaoliu@foxmail.com",
+    #url="https://www.shiyanlou.com/courses/596",
+    license = "MIT",
+    packages=find_packages()
+    )
