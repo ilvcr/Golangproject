bb8db9fcf6af89af13fa72e1f31a00628fb15953
gao <leeyog@163.com>
2019-04-25 23:23:55 +0800

    g

--- pdfhandle/README.md
+++ pdfhandle/README.md
@@ -1,10 +1,10 @@
 ## 技术栈
 
-1. 通过pdfminer解析pdf文件
-2. 再调用谷歌翻译API进行翻译
+1. 使用pdfminer第三方包解析pdf文件(非扫描版)
+2. 再调用GOOGLE翻译API进行翻译
 
 ## 使用流程
 
-1. 将需要解析翻译的PDF放在脚本目录下
+1. 将需解析翻译的PDF放在脚本目录下
 2. 修改file name & target name
-3. 运行`python Pdf2Text.py`生成对应语言的对应txt文本文档
+3. 运行`Pdf2Text.py`生成对应语言的对应txt文本文档
---
+++ pdfhandle/src/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+#*************************************************************************#
+# File Name: __init__.py
+# Author: yoghourt->ilvcr 
+# Mail: liyaoliu@foxmail.com  @@  ilvcr@outlook.com 
+# Created Time: Tue Apr  10 09:51:57 2018
+# Description: 
+#************************************************************************#
+
+
