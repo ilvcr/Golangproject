32fe581071ab10227535354d6187f51c31fa057f
Miao <leeyog@163.com>
2019-04-25 22:33:29 +0800

    g

---
+++ studyscript/translate.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+#*************************************************************************#
+# File Name: translate.py
+# Author: yoghourt->ilvcr 
+# Mail: liyaoliu@foxmail.com  @@  ilvcr@outlook.com 
+# Created Time: Thu Apr 25 22:32:41 2019
+# Description: 
+#************************************************************************#
+
+
---
+++ studyscript/webpdf2text.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+#*************************************************************************#
+# File Name: webpdf2text.py
+# Author: yoghourt->ilvcr 
+# Mail: liyaoliu@foxmail.com  @@  ilvcr@outlook.com 
+# Created Time: Thu Apr 25 22:32:55 2019
+# Description: 
+#************************************************************************#
+
+
